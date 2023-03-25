97dc2785aaf0b0200c0c5a6daa7ef0855918f16d
dalevross <dalevross@gmail.com>
2023-03-25 10:01:26 -0400

    Initial project setup, before any modifications

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tad_amnathrig.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
